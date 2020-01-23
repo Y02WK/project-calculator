c06ab22eec90f5e887d9f5f973a971a536adcfb3
Y02WK <waikinyong@gmail.com>
2020-01-24 00:54:41 +0800

    Implemented a delete function that removes the last number input and reverts display back to 0 if only a single digit is displayed.

--- index.js
+++ index.js
@@ -40,6 +40,21 @@ clearButton.addEventListener('click', () => {
     clearCalc();
 })
 
+delButton.addEventListener('click', () => {
+    deleteLast();
+})
+
 function clearCalc() {
     calcDisplay.textContent = 0;
  }
+
+function deleteLast() {
+    const display = calcDisplay.innerText;
+    if (display != 0) {
+        if (display.length > 1) {
+            calcDisplay.textContent = display.substring(0, display.length -1);
+        } else if (display.length == 1) {
+            calcDisplay.textContent = 0;
+        }
+    }
+}
